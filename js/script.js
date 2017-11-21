937cb52d6e5dd80802324f0da63171ce87d3d957
Quentin McGaw <quentin.mcgaw@gmail.com>
2017-11-20 20:27:05 -0500

    More mobile oriented (hide not working yet(

--- js/script.js
+++ js/script.js
@@ -3,44 +3,20 @@ if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(naviga
 	isMobile = true;
 }
 
-function smooth_scrolling() {
-	$('a').click(function(){
-		$('html, body').animate({
-			scrollTop: $( $(this).attr('href') ).offset().top
-		}, 500);
-		return false;
-	});
+function hideScreen(id) {
+	$("#" + id).removeClass("screen")
+	$("#" + id).addClass("hidden")
 }
 
-function banner_opacity() {
-	var banner_opacity_mouseover = 0.93;
-    var banner_opacity_mouseout = 0.66;
-	var banner = $('#navigation_bar');
-    banner.mouseover(function(){
-		banner.css("opacity", banner_opacity_mouseover);
-    });
-    banner.mouseout(function(){
-		banner.css("opacity", banner_opacity_mouseout);
-    });
-	var b_pos = banner.offset().top;
-	$(window).scroll(function (){ // binds the following to when the user scrolls
-		var y = $(this).scrollTop();
-		if(y == 0){
-			banner_opacity_mouseover = 0.93;
-            banner_opacity_mouseout = 0.66;
-		} else {
-			banner_opacity_mouseover = 0.70;
-            banner_opacity_mouseout = 0.5;
-		}
-		banner.stop().animate({
-						'opacity':banner_opacity_mouseout				
-						},400);
-	});
+function showScreen(id) {
+	$("#" + id).removeClass("hidden")
+	$("#" + id).addClass("screen")
 }
 
 $(document).ready( function() { /* executes first */
 	console.log('document is ready');
-    banner_opacity();
+	//showScreen("bill");
+	//hideScreen("bill");
     if(isMobile){
         console.log('Mobile detected');
     } else {
@@ -49,8 +25,7 @@ $(document).ready( function() { /* executes first */
 });
 
 window.onload = function(){ /* executes secondly */
-    console.log('window is loaded');
-    smooth_scrolling();
+	console.log('window is loaded');
 };
 
 $(window).resize(function() {
