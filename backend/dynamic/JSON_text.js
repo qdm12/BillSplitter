571591bba222500b87b722a6d1cc080e4143dd74
gs2723 <33817592+gs2723@users.noreply.github.com>
2017-12-10 19:39:56 -0500

    Add files via upload

--- backend/dynamic/JSON_text.js
+++ backend/dynamic/JSON_text.js
@@ -1,6 +1,21 @@
 {
+"link":"custom_link_URL",
 "USER":"Gleb Sagitov",
-"billID:"123456789",
-"items:["item1","item2","item3","item4","item5"],
-"otherpeople":["person1","person2","person3","person4","person5"]
+"billID":123456789,
+"itemList":
+		{
+		"item1":{"itemId":42,"itemName":"Pizza","itemAmount":"$15.00","peopleList":
+		 	[{"personID":1,"name":"Person1"},{"personID":2,"name":"Person2"},{"personID":3,"name":"Person3"},{"personID":4,"name":"Person4"},{"personID":5,"name":"Person5"}]},
+		"item2":{"itemId":79,"itemName":"French Fries","itemAmount":"$8.50","peopleList":
+			[{"personID":1,"name":"Person1"},{"personID":2,"name":"Person2"},{"personID":3,"name":"Person3"},{"personID":4,"name":"Person4"},{"personID":5,"name":"Person5"}]},
+		"item3":{"itemId":36,"itemName":"Milk Shake","itemAmount":"$3.75","peopleList":
+			[{"personID":1,"name":"Person1"},{"personID":2,"name":"Person2"},{"personID":3,"name":"Person3"},{"personID":4,"name":"Person4"},{"personID":5,"name":"Person5"}]},
+		"item4":{"itemId":28,"itemName":"Soda Pop","itemAmount":"$2.25","peopleList":
+			[{"personID":1,"name":"Person1"},{"personID":2,"name":"Person2"},{"personID":3,"name":"Person3"},{"personID":4,"name":"Person4"},{"personID":5,"name":"Person5"}]},
+		"item5":{"itemId":67,"itemName":"Chips & Dip","itemAmount":"$12.85","peopleList":
+			[{"personID":1,"name":"Person1"},{"personID":2,"name":"Person2"},{"personID":3,"name":"Person3"},{"personID":4,"name":"Person4"},{"personID":5,"name":"Person5"}]}
+		},
+"vendorName":"Bob's Burrito's",
+"billName":"Team Dinner",
+"time":"2017-12-11T18:25:43-05:00"
 }
