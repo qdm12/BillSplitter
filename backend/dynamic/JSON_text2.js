c34ce9016996aef7be2cc3d732cc027e3f00b856
gs2723 <33817592+gs2723@users.noreply.github.com>
2017-12-15 17:10:24 -0500

    Add files via upload

---
+++ backend/dynamic/JSON_text2.js
@@ -0,0 +1,21 @@
+{"id": 1,
+"time": {"sec":"01", "min":"00", "hour":"00", "day":"01", "month":"Dec", "year":"2017"},
+"address": "196 W Third Avenue",
+"restaurant": "Pizza Oven",
+"name": "Birthday pizza",
+"tax": 19.67,
+"tip": 5,
+"link": "2VxFHtGDh44bMtW4VbngW3XxPQwqIQucnAUM6ZHL",
+"done": 0,
+"users": [{"id":1, "username":"Alice"}, {"id":2, "username":"Bob"}],
+"tempUsers": [{"id":1, "username":"John"}],
+"items": [{"id":1, "name":"PizzaA", "amount":10.5}, {"id":2, "name":"PizzaB", "amount":14}, {"id":3, "name":"Fries", "amount":6.24}],
+"consumers": [
+        {"item_id": 1, "user_id": 1, "temp_user_id": null, "paid": 0 },
+        {"item_id": 1, "user_id": null, "temp_user_id": 1, "paid": 0 },
+        {"item_id": 2, "user_id": 2, "temp_user_id": null, "paid": 0 },
+        {"item_id": 3, "user_id": 1, "temp_user_id": null, "paid": 0 },
+        {"item_id": 3, "user_id": 2, "temp_user_id": null, "paid": 0 },
+        {"item_id": 3, "user_id": null, "temp_user_id": 1, "paid": 0 }
+    ]
+}
