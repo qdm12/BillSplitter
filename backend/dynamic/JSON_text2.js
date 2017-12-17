43a6d709fd8133a67f47613365475fc8d5465610
Benjamin Alpert <bja282@nyu.edu>
2017-12-17 18:17:55 -0500

    Updated CSS for overflow in the case of many items purchased.

--- backend/dynamic/JSON_text2.js
+++ backend/dynamic/JSON_text2.js
@@ -9,7 +9,8 @@
 "done": 0,
 "users": [{"id":1, "username":"Alice Smith"}, {"id":2, "username":"Bob Builder"}],
 "tempUsers": [{"id":1, "username":"John Jacob"}],
-"items": [{"id":1, "name":"Pizza A", "amount":10.5}, {"id":2, "name":"Pizza B", "amount":14}, {"id":3, "name":"Fries", "amount":6.24}],
+"items": [{"id":1, "name":"Pizza A", "amount":10.5}, {"id":2, "name":"Pizza B", "amount":14}, {"id":3, "name":"Fries", "amount":6.24}, {"id":4, "name":"Coca Cola", "amount":3}, {"id":5, "name":"Milk Shake", "amount":4}],
+ 
 "consumers": [
         {"item_id": 1, "user_id": 1, "temp_user_id": null, "paid": 0 },
         {"item_id": 1, "user_id": null, "temp_user_id": 1, "paid": 0 },
